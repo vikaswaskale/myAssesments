bd9e53a3ff3929f1f82300acf7a108e6307302a9
Vikas Waskale <vikaswaskale@gmail.com>
2020-02-09 21:14:30 +0000

    updates

--- calculate/calculate/src/main/java/com/myassesment/calculate/controller/CalculationController.java
+++ calculate/calculate/src/main/java/com/myassesment/calculate/controller/CalculationController.java
@@ -1,13 +1,22 @@
 package com.myassesment.calculate.controller;
 
+import java.util.Date;
+
+import javax.validation.constraints.NotNull;
+
+import org.omg.IOP.ExceptionDetailMessage;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.myassesment.calculate.exception.ApplicationError;
+import com.myassesment.calculate.exception.EceptionMessge;
+
 import io.swagger.annotations.ApiOperation;
 
 @RestController
-public class SimpleCalculationController {
+public class CalculationController
+{
 	
 	    /**
 	     * Get method to provide the calculation of two numbers
@@ -17,10 +26,10 @@ public class SimpleCalculationController {
 	     * @return String results of multiplication
 	     */
 	   
-	@GetMapping (value = "/multiply/{num1},{num2}")
+	@GetMapping (value = "/multiply/{a},{b}")
 	@ApiOperation("Multiplies two numbers passed as path parameters")
-	 public String mul(@PathVariable("num1") Integer num1, @PathVariable("num2") Integer num2) {
-	        return String.format("Multiplicaiton of two numbers is = "+(num1 * num2));
+	 public String multiplyTwoNumbers(@PathVariable("a") @NotNull Integer num1, @PathVariable("b") @NotNull Integer num2)  throws Exception  {
+		        return String.format("Multiplicaiton of two numbers is = "+(num1 * num2));
 	    }
 
 }
--- calculate/calculate/src/test/java/com/myassesment/calculate/CalculateApplicationTests.java
+++ calculate/calculate/src/test/java/com/myassesment/calculate/CalculateApplicationTests.java
@@ -1,13 +1,51 @@
 package com.myassesment.calculate;
 
-import org.junit.jupiter.api.Test;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest
-class CalculateApplicationTests {
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment=WebEnvironment.MOCK, classes={ CalculateApplication.class })
+public class CalculateApplicationTests {
+	
+	private MockMvc mockMvc;
+	
+	  @Autowired
+	  private WebApplicationContext webApplicationContext;
 
-	@Test
-	void contextLoads() {
-	}
+	  @Before
+	  public void setUp() {
+		  this.mockMvc = webAppContextSetup(webApplicationContext).build();
+	  }
+	  
+	 @Test
+	  public void testWhenTwoNumber() throws Exception {
+	    /* setup mock */
+	    mockMvc.perform(get("/multiply/2,5")) 
+	              .andExpect(status().isOk())
+	             .andExpect(content().string("Multiplicaiton of two numbers is = "+10));
+	   	  }
+
+	 
+	 @Test
+	  public void testWhenResultsAreNotCorrect() throws Exception {
+	    /* setup mock */
+	    mockMvc.perform(get("/multiply/2,2")) 
+	              .andExpect(status().isOk());
+	            
+	   	  }
+
+	 
 
 }
