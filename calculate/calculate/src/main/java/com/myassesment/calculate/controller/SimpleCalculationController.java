1078507797cb4a76e125a31e774359fda63d5a40
Vikas Waskale <vikaswaskale@gmail.com>
2020-02-09 12:21:20 +0000

    updates in controller

--- calculate/calculate/src/main/java/com/myassesment/calculate/controller/SimpleCalculationController.java
+++ calculate/calculate/src/main/java/com/myassesment/calculate/controller/SimpleCalculationController.java
@@ -1,29 +1,26 @@
 package com.myassesment.calculate.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import io.swagger.annotations.ApiOperation;
 
 @RestController
 public class SimpleCalculationController {
 	
-	
-	
-
 	    /**
+	     * Get method to provide the calculation of two numbers
+	     * 
 	     * @param num1
 	     * @param num2
-	     * @return String results of multiplicaiton
+	     * @return String results of multiplication
 	     */
-	    @RequestMapping(method = RequestMethod.GET, value = "/mul/{num1}/{num2}", produces = "text/plain")
-	    @ApiOperation("Multiplies two numbers passed as path parameters")
-	    public String mul(@PathVariable("num1") Integer num1, @PathVariable("num2") Integer num2) {
-	        return String.format("%d x %d = %d", num1, num2, (num1 * num2));
+	   
+	@GetMapping (value = "/multiply/{num1},{num2}")
+	@ApiOperation("Multiplies two numbers passed as path parameters")
+	 public String mul(@PathVariable("num1") Integer num1, @PathVariable("num2") Integer num2) {
+	        return String.format("Multiplicaiton of two numbers is = "+(num1 * num2));
 	    }
-
 
 }
