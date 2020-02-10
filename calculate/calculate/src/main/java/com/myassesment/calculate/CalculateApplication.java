f4f1ac82278a962c0fa12d6b59e0fedc7f1f6fc5
Vikas Waskale <vikaswaskale@gmail.com>
2020-02-10 22:27:41 +0000

    final

--- calculate/calculate/src/main/java/com/myassesment/calculate/CalculateApplication.java
+++ calculate/calculate/src/main/java/com/myassesment/calculate/CalculateApplication.java
@@ -2,8 +2,10 @@ package com.myassesment.calculate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CalculateApplication {
 
 	public static void main(String[] args) {
--- calculate/calculate/src/main/java/com/myassesment/calculate/controller/CalculationController.java
+++ calculate/calculate/src/main/java/com/myassesment/calculate/controller/CalculationController.java
@@ -1,18 +1,10 @@
 package com.myassesment.calculate.controller;
 
-import java.util.Date;
-
 import javax.validation.constraints.NotNull;
 
-import org.omg.IOP.ExceptionDetailMessage;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.myassesment.calculate.exception.ApplicationError;
-import com.myassesment.calculate.exception.EceptionMessge;
-
-import io.swagger.annotations.ApiOperation;
 
 @RestController
 public class CalculationController
@@ -27,8 +19,7 @@ public class CalculationController
 	     */
 	   
 	@GetMapping (value = "/multiply/{a},{b}")
-	@ApiOperation("Multiplies two numbers passed as path parameters")
-	 public String multiplyTwoNumbers(@PathVariable("a") @NotNull Integer num1, @PathVariable("b") @NotNull Integer num2)  throws Exception  {
+	 public String multiplyTwoNumbers(@PathVariable("a") @NotNull Integer num1, @PathVariable("b") @NotNull Integer num2){
 		        return String.format("Multiplicaiton of two numbers is = "+(num1 * num2));
 	    }
 
